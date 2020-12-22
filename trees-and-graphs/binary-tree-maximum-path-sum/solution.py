b8f470428a7c1c6ceee2c4c4b613954871cd0e30
Allan Delarosa <ajd2215@columbia.edu>
2020-12-22 15:13:11 -0500

    Binary tree max path sum iterative solution

--- trees-and-graphs/binary-tree-maximum-path-sum/solution.py
+++ trees-and-graphs/binary-tree-maximum-path-sum/solution.py
@@ -11,16 +11,28 @@ class Solution(object):
         :rtype: int
         """
         
-        def helper(root, max_sum):
-            if not root:
-                return 0
-            
-            left = helper(root.left, max_sum)
-            right = helper(root.right, max_sum)
-            
-            max_sum[0] = max(max_sum[0], root.val + max(left, 0) + max(right, 0))
-            return root.val + max(0, left, right)
+        max_sum = -2147483648
         
-        max_sum = [-2147483648]
-        helper(root, max_sum)
-        return max_sum[0]
+        # on stack, push reference to current node, sum from the left, and boolean to know if ready to update max sum
+        stack = []
+        curr = root
+        
+        while stack or curr:
+            while curr:
+                curr_sum = 0
+                stack.append((curr, 0, False))
+                curr = curr.left
+                
+            curr, left_sum, ready = stack.pop()
+            if ready:
+                right_sum = max(curr_sum, 0)
+                max_sum = max(max_sum, curr.val + left_sum + right_sum)
+                curr_sum = curr.val + max(left_sum, right_sum)
+                curr = None
+            else:
+                left_sum = max(curr_sum, 0)
+                stack.append((curr, left_sum, True))
+                curr_sum = 0
+                curr = curr.right
+                
+        return max_sum
