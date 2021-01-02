9f8fa4e98c12dbe01f11cbd5680208b4a44d2fbd
Allan Delarosa <ajd2215@columbia.edu>
2021-01-02 18:30:51 -0500

    K closest points to origin quickselect solution and explanation

--- sorting-and-searching/k-closest-points/solution.py
+++ sorting-and-searching/k-closest-points/solution.py
@@ -6,14 +6,33 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        closest = []
+        dist = lambda i: points[i][0] ** 2 + points[i][1] ** 2
         
-        for p in points:
-            distance = sqrt(p[0] ** 2 + p[1] ** 2)
+        oi, oj = 0, len(points) - 1
+        
+        while oi < oj:
+            i, j = oi, oj
+            k = randint(i, j)
+            pivot = dist(k)
+            points[i], points[k] = points[k], points[i]
+            i += 1
             
-            if len(closest) == K:
-                heapq.heappushpop(closest, (-distance, p))
-            else:
-                heapq.heappush(closest, (-distance, p))
+            while True:
+                while i < j and dist(i) < pivot:
+                    i += 1
+                while i <= j and dist(j) >= pivot:
+                    j -= 1
+                if i >= j:
+                    break
+                points[i], points[j] = points[j], points[i]
                 
-        return [p[1] for p in closest]
+            points[oi], points[j] = points[j], points[oi]
+            
+            if K < j + 1:
+                oj = j - 1
+            elif K > j + 1:
+                oi = j + 1
+            else:
+                break
+                    
+        return points[:K]
