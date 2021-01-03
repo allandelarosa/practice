047f4e53e1e2f80faa877fd32c50210a6bfe734e
Allan Delarosa <ajd2215@columbia.edu>
2021-01-02 19:38:12 -0500

    Top k frequent elements quickselect solution

--- sorting-and-searching/top-k-frequent-elements/solution.py
+++ sorting-and-searching/top-k-frequent-elements/solution.py
@@ -7,12 +7,31 @@ class Solution(object):
         """
         
         counts = Counter(nums)
+        unique = counts.keys()
         
-        h = []
-        for num, count in counts.items():
-            if len(h) == k:
-                heapq.heappushpop(h, (count, num))
+        oi, oj = 0, len(unique) - 1
+        while oi < oj:
+            i, j, l = oi, oj, randint(oi, oj)
+            unique[i], unique[l] = unique[l], unique[i]
+            
+            pivot = counts[unique[i]]
+            i += 1
+            
+            while True:
+                while i < j and counts[unique[i]] > pivot:
+                    i += 1
+                while i <= j and counts[unique[j]] <= pivot:
+                    j -= 1
+                if i >= j:
+                    break
+                unique[i], unique[j] = unique[j], unique[i]
+            unique[oi], unique[j] = unique[j], unique[oi]
+            
+            if k > j + 1:
+                oi = j + 1
+            elif k < j + 1:
+                oj = j - 1
             else:
-                heapq.heappush(h, (count, num))
+                break
         
-        return [num for count, num in h]
+        return unique[:k]
